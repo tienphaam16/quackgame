6ee1f6f66f9fdb04f392212709ee0d0048a31038
mhqb365 <mhqb365@gmail.com>
2024-06-05 07:11:07 +0700

    add argument

--- quack.js
+++ quack.js
@@ -1,8 +1,18 @@
 const loadToken = require("./modules/loadToken");
 const ACCESS_TOKEN = loadToken();
 
-const harvestEggGoldenDuck = require("./scripts/harvestEggGoldenDuck");
-// const collectGoldenDuck = require("./scripts/collectGoldenDuck");
+const args = process.argv;
+// console.log(args);
+const script = args[2];
 
-harvestEggGoldenDuck(ACCESS_TOKEN);
-// collectGoldenDuck(ACCESS_TOKEN);
+const harvestEggGoldenDuck = require("./scripts/harvestEggGoldenDuck");
+const collectGoldenDuck = require("./scripts/collectGoldenDuck");
+
+switch (script) {
+  case "1":
+    collectGoldenDuck(ACCESS_TOKEN);
+    break;
+  default:
+    harvestEggGoldenDuck(ACCESS_TOKEN);
+    break;
+}
